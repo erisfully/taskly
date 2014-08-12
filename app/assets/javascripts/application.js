f69d92274219380ae8b7008fafd1dd09bb533899
Ashley <a.clarkem@gmail.com>
2014-08-12 15:36:32 -0600

    tasks can be toggled on

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+
+
+  var button = $('button')
+    $(button).click(function() {
+      $('.tasks').addClass('show')
+  });
+
+
+
+});
