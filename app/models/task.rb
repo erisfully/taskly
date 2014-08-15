62227bf43d6821c779977e3df9549e53fe3232b7
Ashley <a.clarkem@gmail.com>
2014-08-15 11:19:40 -0600

    updated task and user models to reflect new user_id column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
 
   belongs_to :task_list
+  belongs_to :user
   validates :task, :presence => true
   validates :task_list_id, :presence => true
   validates :date, :presence => true
